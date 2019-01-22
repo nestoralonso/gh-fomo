1bfeacb8e19eb50540723944cb81fb7338115dad
Nestor Arias <nestor.arias@prodigious.com>
2019-01-21 22:01:25 -0500

    dummy change

--- src/FollowingStarred.js
+++ src/FollowingStarred.js
@@ -71,7 +71,7 @@ const User = React.memo(function User(props) {
   if (!nameSanitized) {
     nameSanitized = userName;
   }
-  const revRepos = repos.slice(0).reverse();
+  const revRepos = [...repos].reverse();
   return (
     <div className="person-with-repos">
       <img className="person-avatar" width="64" height="64" src={props.avatarUrl} alt={nameSanitized} />
